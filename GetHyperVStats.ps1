a1ee59bc44108dc7f14c7dee5d2d251418cbc449
Florent Appointaire <florent@Florents-MBP.lan>
2017-09-20 08:28:39 +0200

    Second release

--- GetHyperVStats.ps1
+++ GetHyperVStats.ps1
@@ -6,16 +6,18 @@ function Get-ComputerStats {
      [string[]]$ComputerName
    )
 
+$creds = Get-AutomationPSCredential -Name 'OnPrem Admin Account'
+
    Write-Output "The script is starting..."
      foreach ($c in $ComputerName) {
-         $avg = Get-WmiObject win32_processor -computername $c |
+         $avg = Get-WmiObject win32_processor -computername $c -Credential $creds |
                     Measure-Object -property LoadPercentage -Average |
                     Foreach {$_.Average}
-         $mem = Get-WmiObject win32_operatingsystem -ComputerName $c |
+         $mem = Get-WmiObject win32_operatingsystem -ComputerName $c -Credential $creds |
                     Foreach {"{0:N2}" -f ((($_.TotalVisibleMemorySize - $_.FreePhysicalMemory)*100)/ $_.TotalVisibleMemorySize)}
-         $freeC = Get-WmiObject Win32_Volume -ComputerName $c -Filter "DriveLetter = 'C:'" |
+         $freeC = Get-WmiObject Win32_Volume -ComputerName $c -Filter "DriveLetter = 'C:'" -Credential $creds |
                      Foreach {"{0:N2}" -f (($_.FreeSpace / $_.Capacity)*100)}
-	 $freeH = Get-WmiObject Win32_Volume -ComputerName $c -Filter "DriveLetter = 'H:'" |
+	 $freeH = Get-WmiObject Win32_Volume -ComputerName $c -Filter "DriveLetter = 'H:'" -Credential $creds |
                      Foreach {"{0:N2}" -f (($_.FreeSpace / $_.Capacity)*100)}
           [pscustomobject] [ordered] @{
              ComputerName = $c
@@ -28,4 +30,5 @@ function Get-ComputerStats {
 
 }
 
+$hpvHost = Get-AutomationVariable -Name 'HyperV-Hosts'
 Get-ComputerStats -ComputerName FLOAPP-HPV01 | FT
